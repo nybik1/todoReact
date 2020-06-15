eada5383b93722e85250954a2b120e2eee9c9a07
nybik1 <spudegbutman@gmail.com>
2020-06-15 20:03:31 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import ToDoInput from './components/ToDoInput';
 import List from './components/List';
 
+
 class App extends React.Component {
   state = {
-    todos: []
+    todos: [],
+    filterBy: 'all',
   }
 
   handleTodo = (value) => {
@@ -35,13 +37,40 @@ class App extends React.Component {
     })
   }
 
-  render() {
-    const { todos } = this.state;
+  setFilterBy = (filterBy) => () => {
+    this.setState({
+      filterBy
+    })
+  }
 
+  filterTodos() {
+    const { todos, filterBy } = this.state
+    switch (filterBy) {
+      case 'active': {
+        return todos.filter(item => !item.completed)
+      }
+      case 'completed': {
+        return todos.filter(item => item.completed)
+      }
+      default:
+        return todos;
+    }
+  }
+
+  clearTodos = () => {
+    this.setState({ todos: this.state.todos.filter(item => !item.completed) })
+  }
+
+
+  render() {
     return (
       <div className="App" >
         <ToDoInput addTodo={this.handleTodo} />
-        <List toggleCompleted={this.toggleCompleted} items={todos} />
+        <List toggleCompleted={this.toggleCompleted} items={this.filterTodos()} />
+        <button onClick={this.setFilterBy('all')}>All</button>
+        <button onClick={this.setFilterBy("completed")}>completed</button>
+        <button onClick={this.setFilterBy("active")}>active</button>
+        <button onClick={this.clearTodos}>clear todos</button>
       </div>
     );
   }
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
 
-const handleClick = (props) = (e) => {
-    props.onClick(id)
-};
 
 function ListItem({
-    item: { id, value, completed },
-    onClick
+    item: { id, value, completed }, onClick
 }) {
     const styles = {
         ...(completed && { textDecoration: 'line-through' })
     };
     return (
-        <li onClick={handleClick(props)} style={styles}>{value}</li>
+        <li onClick={() => onClick(id)} style={styles} > {value}</li >
     );
 }
 export default ListItem;
